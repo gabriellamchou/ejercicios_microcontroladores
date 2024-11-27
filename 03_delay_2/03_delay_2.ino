ecb90c64c08c761be470991c6abc2a531eb7680a
Gabriel <gabriel.lamela.ch@gmail.com>
2024-11-27 23:05:47 +0100

    Corregir código repetido en 03_delay_2

--- 03_delay_2/03_delay_2.ino
+++ 03_delay_2/03_delay_2.ino
@@ -10,11 +10,15 @@ void loop() {
   for (int i = 0; i < 20; i++) {
     delay(100);
     ctr1++; // cada 0.1seg incrementamos ctr1
-    Serial.println("C1: " + String(ctr1) + " C2: " + String(ctr2));
+    printCounters(ctr1, ctr2);
   }
 
   delay(2000);
   ctr2++; // cada 2seg incrementamos ctr2
   ctr1 -= ctr2;
+  printCounters(ctr1, ctr2);
+}
+
+void printCounters(int ctr1, int ctr2) {
   Serial.println("C1: " + String(ctr1) + " C2: " + String(ctr2));
 }
